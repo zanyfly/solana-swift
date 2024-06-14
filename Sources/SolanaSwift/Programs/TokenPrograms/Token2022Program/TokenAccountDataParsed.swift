24e62b1ab54bedcd8e36577fead366a1256bc6c1
ivanzeng <zanyfly@126.com>
2024-06-14 11:30:15 +0800

    resolve the amount\decimal of token account

--- Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/TokenAccountDataParsed.swift
+++ Sources/SolanaSwift/Programs/TokenPrograms/Token2022Program/TokenAccountDataParsed.swift
@@ -1,7 +1,11 @@
 public struct TokenAccountDataParsed: TokenAccountParse {
+
     public let mint: String
     public let owner: String
     public let program: String
+    public let amount: String
+    public let decimals: Int
+
     enum CodingKeys: CodingKey {
         case program
         case parsed
@@ -11,6 +15,11 @@ public struct TokenAccountDataParsed: TokenAccountParse {
             enum InfoCodingKeys: String, CodingKey {
                 case mint
                 case owner
+                case tokenAmount
+                enum TokenAmountCodingKeys: String, CodingKey {
+                    case amount
+                    case decimals
+                }
             }
         }
     }
@@ -19,9 +28,13 @@ public struct TokenAccountDataParsed: TokenAccountParse {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let parsed = try container.nestedContainer(keyedBy: CodingKeys.ParsedCodingKeys.self, forKey: .parsed)
         let info = try parsed.nestedContainer(keyedBy: CodingKeys.ParsedCodingKeys.InfoCodingKeys.self, forKey: .info)
+        let tokenAmount = try info.nestedContainer(keyedBy: CodingKeys.ParsedCodingKeys.InfoCodingKeys.TokenAmountCodingKeys.self, forKey: .tokenAmount)
+
 
         self.mint = try info.decode(String.self, forKey: .mint)
         self.owner = try info.decode(String.self, forKey: .owner)
         self.program = try container.decode(String.self, forKey: .program)
+        self.amount = try tokenAmount.decode(String.self, forKey: .amount)
+        self.decimals = try tokenAmount.decode(Int.self, forKey: .decimals)
     }
 }
